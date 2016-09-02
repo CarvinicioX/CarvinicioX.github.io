fe598bfb5fe1a247cf4640f23f92a49bd6071661
CarvinicioX <carvinicio@yahoo.com>
2016-09-01 22:43:18 -0600

    Playing

--- js/mine.js
+++ js/mine.js
@@ -3,8 +3,8 @@ function Nav() {
     console.log(document.getElementById("navOpenCloseButton").innerHTML);
     if (document.getElementById("navOpenCloseButton").innerHTML.charAt(0) === 'O') {
         console.log("OPEN");
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
+        document.getElementById("mySidenav").style.width = "175px";
+        document.getElementById("main").style.marginLeft = "175px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
         document.getElementById("navOpenCloseButton").innerHTML= "CLOSE";
     } else if (document.getElementById("navOpenCloseButton").innerHTML.charAt(0) === 'C') {
