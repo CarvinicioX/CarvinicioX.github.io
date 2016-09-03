16b562c244813737e42aff72aea0967c4ecd9c72
CarvinicioX <carvinicio@yahoo.com>
2016-09-02 23:42:51 -0600

    Added Music

--- js/mine.js
+++ js/mine.js
@@ -1,17 +1,41 @@
 function Nav() {
-    console.log("Fuck");
-    console.log(document.getElementById("navOpenCloseButton").innerHTML);
     if (document.getElementById("navOpenCloseButton").innerHTML.charAt(0) === 'O') {
-        console.log("OPEN");
         document.getElementById("mySidenav").style.width = "175px";
         document.getElementById("main").style.marginLeft = "175px";
+        document.getElementById("player").style.marginLeft = "175px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
         document.getElementById("navOpenCloseButton").innerHTML= "CLOSE";
     } else if (document.getElementById("navOpenCloseButton").innerHTML.charAt(0) === 'C') {
-        console.log("CLOSE");
         document.getElementById("mySidenav").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("player").style.marginLeft = "0";
         document.body.style.backgroundColor = "white";
         document.getElementById("navOpenCloseButton").innerHTML = "OPEN";
     }
+}
+
+function email() {
+    if (document.getElementById("sendMail").style.visibility === "visible") {
+        document.getElementById("sendMail").style.visibility = "hidden";
+        document.getElementById("sendMail").style.height = "0px";
+    } else if (document.getElementById("sendMail").style.visibility = "hidden") {
+        document.getElementById("sendMail").style.visibility = "visible";
+        document.getElementById("sendMail").style.height = "175px";
+    }
+}
+
+function play() {
+    
+}
+
+function publish() {
+    
+}
+
+function attach() {
+    
+}
+
+function sendMailHover() {
+    
 }
